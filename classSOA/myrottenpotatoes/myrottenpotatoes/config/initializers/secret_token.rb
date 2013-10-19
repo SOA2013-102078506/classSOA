96f90a2bf56b0cab36aaa780a4ef8aebb9c691e1
saasbook <saasbook@saasbook.(none)>
2013-10-19 01:09:41 -0700

    remove extra dir

--- classSOA/myrottenpotatoes/myrottenpotatoes/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Myrottenpotatoes::Application.config.secret_token = 'dcc937105c33bc88f2e30888e4b4f3cc26bd30dca65668b9c1aee40f6da54c4d2b93bfcf9901fd516cf69d612550158a5516460a70f7df0f3b85f7dbfcac37b3'
